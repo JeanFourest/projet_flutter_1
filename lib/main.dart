40583d0405f722ba22bed3fd8bbb7a628fce4d8d
Floria <florian.richet2004@gmail.com>
2023-09-13 11:22:30 +0200

    tkt

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projet_flutter_1/pages/homePage.dart';
+import 'package:projet_flutter_1/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
