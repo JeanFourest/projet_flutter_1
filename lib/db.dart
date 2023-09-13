01105f006e16ba9c7129fdbec91d5de97098008e
kvong92 <vongkevin.c92@gmail.com>
2023-09-13 12:28:45 +0200

    d

--- lib/db.dart
+++ lib/db.dart
@@ -1,8 +1,10 @@
 import 'package:mongo_dart/mongo_dart.dart';
 
+
 class MongoDatabase {
   static Future<void> connect() async {
     try {
+      //Connexion a la DB
       var db = await Db.create('mongodb+srv://cavalier01:cavalier01@fluttercavalier.vik0exh.mongodb.net/');
       await db.open();
       print('Connexion à MongoDB réussie');
@@ -10,4 +12,6 @@ class MongoDatabase {
       print('Erreur de connexion à MongoDB: $e');
     }
   }
+
+
 }
