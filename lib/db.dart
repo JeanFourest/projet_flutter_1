f6e6802b10d5878cd71001c337fe0383488e8a34
Floria <florian.richet2004@gmail.com>
2023-09-14 19:58:00 +0200

    v2 Edit profil toutes requettes marches

--- lib/db.dart
+++ lib/db.dart
@@ -48,4 +48,113 @@ class MongoDatabase {
       print('Erreur lors de la création du compte: $e');
     }
   }
+
+  // ------------------------------REQUETE-MONGODB-EDIT-PROFIL------------------------------\\
+
+  // ----------REQUETE-MONGODB-EDIT-UPDATE-USER----------
+  static Future<void> updateUserUsername(
+      String userId, String usernameEdited) async {
+    try {
+      await userCollection.update(
+        where.id(ObjectId.parse(
+            userId)), // Obligez d'utiliser ça parceque c des object id ('0000')
+        modify.set('username', usernameEdited),
+      );
+    } catch (e) {
+      print('Erreur lors de la mise à jour du nom d\'utilisateur: $e');
+    }
+  }
+
+  // ----------REQUETE-MONGODB-EDIT-UPDATE-PHONE----------
+  static Future<void> updateUserPhone(String userId, String phoneEdited) async {
+    try {
+      await userCollection.update(
+        where.id(ObjectId.parse(
+            userId)), // Obligez d'utiliser ça parceque c des object id ('0000')
+        modify.set('phoneNumber', phoneEdited),
+      );
+    } catch (e) {
+      print('Erreur lors de la mise à jour du nom d\'utilisateur: $e');
+    }
+  }
+
+  // ----------REQUETE-MONGODB-EDIT-UPDATE-EMAIL----------
+  static Future<void> updateUserEmail(String userId, String emailEdited) async {
+    try {
+      await userCollection.update(
+        where.id(ObjectId.parse(
+            userId)), // Obligez d'utiliser ça parceque c des object id ('0000')
+        modify.set('email', emailEdited),
+      );
+    } catch (e) {
+      print('Erreur lors de la mise à jour du nom d\'utilisateur: $e');
+    }
+  }
+
+  // ----------REQUETE-MONGODB-EDIT-UPDATE-PHOTO----------
+  static Future<void> updateUserPhoto(String userId, String photoEdited) async {
+    try {
+      await userCollection.update(
+        where.id(ObjectId.parse(
+            userId)), // Obligez d'utiliser ça parceque c des object id ('0000')
+        modify.set('photo', photoEdited),
+      );
+    } catch (e) {
+      print('Erreur lors de la mise à jour du nom d\'utilisateur: $e');
+    }
+  }
+
+  // ----------REQUETE-MONGODB-EDIT-UPDATE-PASSWORD----------
+  static Future<void> updateUserPassword(
+      String userId, String passwordEdited) async {
+    try {
+      await userCollection.update(
+        where.id(ObjectId.parse(
+            userId)), // Obligez d'utiliser ça parceque c des object id ('0000')
+        modify.set('password', passwordEdited),
+      );
+    } catch (e) {
+      print('Erreur lors de la mise à jour du nom d\'utilisateur: $e');
+    }
+  }
+
+  // ----------REQUETE-MONGODB-EDIT-UPDATE-AGE----------
+  static Future<void> updateUserAge(String userId, String AgeEdited) async {
+    try {
+      await userCollection.update(
+        where.id(ObjectId.parse(
+            userId)), // Obligez d'utiliser ça parceque c des object id ('0000')
+        modify.set('age', AgeEdited),
+      );
+    } catch (e) {
+      print('Erreur lors de la mise à jour du nom d\'utilisateur: $e');
+    }
+  }
+
+  // ----------REQUETE-MONGODB-EDIT-UPDATE-FFE----------
+  static Future<void> updateUserFFE(String userId, String FFEEdited) async {
+    try {
+      await userCollection.update(
+        where.id(ObjectId.parse(
+            userId)), // Obligez d'utiliser ça parceque c des object id ('0000')
+        modify.set('ffe', FFEEdited),
+      );
+    } catch (e) {
+      print('Erreur lors de la mise à jour du nom d\'utilisateur: $e');
+    }
+  }
+
+  // ----------REQUETE-MONGODB-EDIT-UPDATE-DP----------
+  static Future<void> updateUserDP(String userId, String DPEdited) async {
+    try {
+      await userCollection.update(
+        where.id(ObjectId.parse(
+            userId)), // Obligez d'utiliser ça parceque c des object id ('0000')
+        modify.set('dp', DPEdited),
+      );
+    } catch (e) {
+      print('Erreur lors de la mise à jour du nom d\'utilisateur: $e');
+    }
+  }
+  // ------------------------------REQUETE-MONGODB-EDIT-PROFIL------------------------------\\
 }
